94dd1173980cf34bffdafb0581073a484237906c
ficok <fi.antanaskovic@gmail.com>
2025-04-10 09:22:55 +0200

    Add UnitOfWork pattern interface and class.

---
+++ PersonalFinanceTracker/data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalFinanceTracker.Data.Repositories;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Data.UnitOfWork
+    /** Interface for the Unit of work pattern. Enforces the UnitOfWork class to
+     *  define the CommitAsync method and to hold the Queriers. */
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IQuerier<Account> Accounts { get; }
+        IQuerier<Transaction> Transactions { get; }
+        IQuerier<RecurringTransaction> RecurringTransactions { get; }
+        IQuerier<Category> Categories { get; }
+
+        Task<int> CommitAsync();
+    }
+}
---
+++ PersonalFinanceTracker/data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinanceTracker.Data.Context;
+using PersonalFinanceTracker.Data.Repositories;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Data.UnitOfWork
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly Database db_;
+
+        public IQuerier<Account> Accounts { get; }
+        public IQuerier<Transaction> Transactions { get; }
+        public IQuerier<RecurringTransaction> RecurringTransactions { get; }
+        public IQuerier<Category> Categories { get; }
+
+        public UnitOfWork(Database db)
+        {
+            db_ = db;
+
+            Accounts = new Querier<Account>(db_);
+            Transactions = new Querier<Transaction>(db_);
+            RecurringTransactions = new Querier<RecurringTransaction>(db_);
+            Categories = new Querier<Category>(db_);
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await db_.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            db_.Dispose();
+        }
+
+
+    }
+}
