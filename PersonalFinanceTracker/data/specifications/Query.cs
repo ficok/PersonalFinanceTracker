66d753a7bb62840110a2d2d13ef7f9de7835dcc3
ficok <fi.antanaskovic@gmail.com>
2025-04-14 17:49:16 +0100

    Update queries: custom queries with custom conditions.

---
+++ PersonalFinanceTracker/data/specifications/Query.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceTracker.Data.Specifications
+{
+    /** This is a base Query class - an abstract class. It is a template for the 
+     *  actual queries that shows how they should implement the IQuery interface.
+     */
+    public class Query<T>: IQuery<T>
+    {
+        public List<Expression<Func<T, bool>>> Conditions { get; } = new List<Expression<Func<T, bool>>>();
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public void AddCondition(Expression<Func<T, bool>> condition)
+        {
+            Conditions.Add(condition);
+        }
+        public void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
